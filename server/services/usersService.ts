89140d995b0df06ec0f85f36d658f8ba4f744072
TehilaShaulzon <t8472959@gmail.com>
2024-07-14 10:59:11 +0300

    add add user function in user service

--- server/services/usersService.ts
+++ server/services/usersService.ts
@@ -1,44 +1,41 @@
-// import { User } from '../models/user';
-// import jwt from "jsonwebtoken";
-// import bcrypt from "bcryptjs";
-// import sequelize from "../dataAccess/dataAccess";
-// async function addUser(user:User) {
-//     user.password = await bcrypt.hash(user.password, 10);
-//     const token = jwt.sign(
-//         { user_id: user.id, name: user.name },
-//         "1234567890",
-//         {
-//             expiresIn: "2h",
-//         }
-//     );
-//     try {
-//         const existingUser = await User.findOne({
-//             where: {
-//                 name: user.name,
-//             }
-//         });
-//         if (existingUser) {
-//             throw new Error('User with the same name already exists');
-//         }
-//         const maxId = await User.max('id');
-//         user.id = maxId + 1;
-//         sequelize.authenticate().then(async () => {
-//             await User.sync();
-//             const newUser = await User.create({
-//                 id: user.id,
-//                 name: user.name,
-//                 password: user.password,
-//                 email: user.email,
-//                 token: token
-//             });
-//         });
-//         return `data added: ${JSON.stringify(user)}`;
-//     }
-//     catch (error) {
-//         console.error('Error adding user:', error.message);
-//         return `Error adding user: ${error.message}`;
-//     }
-// }
+import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import sequelize from "../dataAccess/dataAccess";
+import { CustomError } from '../errors/CustomError';
+export async function addUser(user: User) {
+    user.userPassword = await bcrypt.hash(user.userPassword, 10);
+    const token = jwt.sign(
+        { user_id: user.id, email: user.userEmail },
+        "1234567890",
+        {
+            expiresIn: "2h",
+        }
+    );
+
+    const existingUser = await User.findOne({
+        where: {
+            name: user.userName,
+        }
+    });
+    if (existingUser) {
+        throw new CustomError('User with the same name already exists', 400);
+    }
+
+    await sequelize.authenticate()
+    await User.sync();
+    const newUser = await User.create({
+        name: user.userName,
+        password: user.userPassword,
+        email: user.userEmail,
+        token: token
+    });
+
+   const result = await User.findByPk(newUser.id, {
+        attributes: { exclude: ['createdAt', 'updatedAt'] }
+    });
+    return result;
+}
 // async function login(loginName, loginPassword) {
 //     try {
 //         const users = await User.findAll({
