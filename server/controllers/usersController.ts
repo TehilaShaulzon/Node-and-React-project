de71f897b1e7e662f0b761aab09113cf184ed9b5
TehilaShaulzon <t8472959@gmail.com>
2024-07-15 18:44:23 +0300

    removing וnnecessary

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -5,27 +5,6 @@ import express from 'express';
 import { addUser, login } from "../services/usersService";
 import { CustomError } from "../errors/CustomError";
 const router = express.Router();
-// router.get('/', async (req: Request, res: Response) => {
-//     try {
-//         const users = await User.findOne({
-//             where: {
-//                 id: 3
-//             },
-//             raw: true
-//         })
-        
-//         if (!users) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-
-//         res.status(200).json(users);
-//     } catch (err) {
-       
-//             res.status(500).json({ message: 'Internal server error' });
-       
-//         console.error('Error:', err);
-//     }
-// });
 
 router.post('/signUp', async (req: Request, res: Response) => {
     try {
