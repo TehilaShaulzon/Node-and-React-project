cfbe5aba4ddcc5c705119eec05c91ed3cc1feec9
TehilaShaulzon <t8472959@gmail.com>
2024-07-15 16:06:21 +0300

    add login

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -2,25 +2,55 @@ import { User } from "../models/user";
 import { Request, Response } from 'express';
 
 import express from 'express';
+import { addUser, login } from "../services/usersService";
+import { CustomError } from "../errors/CustomError";
 const router = express.Router();
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const users = await User.findOne({
-            where: {
-                id: 3
-            },
-            raw: true
-        })
+// router.get('/', async (req: Request, res: Response) => {
+//     try {
+//         const users = await User.findOne({
+//             where: {
+//                 id: 3
+//             },
+//             raw: true
+//         })
         
-        if (!users) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+//         if (!users) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
 
-        res.status(200).json(users);
-    } catch (err) {
+//         res.status(200).json(users);
+//     } catch (err) {
        
-            res.status(500).json({ message: 'Internal server error' });
+//             res.status(500).json({ message: 'Internal server error' });
        
+//         console.error('Error:', err);
+//     }
+// });
+
+router.post('/signUp', async (req: Request, res: Response) => {
+    try {
+        const result = await addUser(req.body);
+        res.status(201).json(result);
+    } catch (err: any) {
+        if (err instanceof CustomError) {
+            res.status(err.statusCode).send(err.message);
+        } else {
+            res.status(500).send('Internal Server Error');
+        }
+        console.error('Error:', err);
+    }
+});
+router.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { loginEmail, loginPassword } = req.body;
+        const result = await login(loginEmail, loginPassword);
+        res.status(200).json(result);
+    } catch (err: any) {
+        if (err instanceof CustomError) {
+            res.status(err.statusCode).send(err.message);
+        } else {
+            res.status(500).send('Internal Server Error');
+        }
         console.error('Error:', err);
     }
 });
--- server/services/usersService.ts
+++ server/services/usersService.ts
@@ -3,7 +3,13 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import sequelize from "../dataAccess/dataAccess";
 import { CustomError } from '../errors/CustomError';
+import _ from 'lodash';
+
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET;
 export async function addUser(user: User) {
+    console.log(user);
+    
     user.userPassword = await bcrypt.hash(user.userPassword, 10);
     const token = jwt.sign(
         { user_id: user.id, email: user.userEmail },
@@ -15,56 +21,58 @@ export async function addUser(user: User) {
 
     const existingUser = await User.findOne({
         where: {
-            name: user.userName,
+            userEmail: user.userEmail,
         }
     });
     if (existingUser) {
-        throw new CustomError('User with the same name already exists', 400);
+        throw new CustomError('User with the same email already exists', 400);
     }
 
     await sequelize.authenticate()
     await User.sync();
     const newUser = await User.create({
-        name: user.userName,
-        password: user.userPassword,
-        email: user.userEmail,
-        token: token
+        userName: user.userName,
+        userPassword: user.userPassword,
+        userEmail: user.userEmail,
+        userToken: token
     });
 
-   const result = await User.findByPk(newUser.id, {
-        attributes: { exclude: ['createdAt', 'updatedAt'] }
-    });
-    return result;
+   const result = await User.findByPk(newUser.id);
+    const userWithoutPassword = _.omit(result?.dataValues, ['userPassword','userToken','updatedAt','createdAt']);
+
+    return { user: userWithoutPassword, token: result?.userToken };
 }
-// async function login(loginName, loginPassword) {
-//     try {
-//         const users = await User.findAll({
-//             where: {
-//                 name: loginName
-//             }
-//         });
-//         if (users.length === 0) {
-//             throw new Error('No user found with the given name');
-//         }
-//         console.log("users " + users);
-//         const user = users.find(u => bcrypt.compareSync(loginPassword, u.password));
-//         if (!user) {
-//             throw new Error('Invalid password');
-//         }
-//         const newToken = jwt.sign(
-//             { user_id: user.id, name: user.name },
-//             "1234567890",
-//             {
-//                 expiresIn: "2h",
-//             }
-//         );
-//         const updatedUser = await User.update({ token: newToken }, {
-//             where: { id: user.id }
-//         });
-//         return { user: user, token: newToken };
-//     } catch (err) {
-//         console.error('Error authenticating user', err);
-//         throw err;
-//     }
-// }
-// module.exports = { addUser, login };
+export async function login(loginEmail:string, loginPassword:string) {
+    
+    const foundUser = await User.findOne({
+        where: {
+            userEmail: loginEmail
+        }
+    });
+
+    if (!foundUser) {
+        throw new CustomError('No user found with the given email', 404);
+    }
+
+    const passwordMatch = bcrypt.compareSync(loginPassword, foundUser.userPassword);
+    if (!passwordMatch) {
+        throw new CustomError('Invalid password', 401);
+    }
+
+    const newToken = jwt.sign(
+        { user_id: foundUser.id, email: foundUser.userEmail },
+        `${JWT_SECRET}`,
+        {
+            expiresIn: "2h",
+        }
+    );
+
+    await User.update({ token: newToken }, {
+        where: { id: foundUser.id }
+    });
+
+    const userWithoutPassword = _.omit(foundUser.dataValues, ['userPassword','userToken','createdAt','updatedAt']);
+
+    return { user: userWithoutPassword, token: newToken };
+   
+}
