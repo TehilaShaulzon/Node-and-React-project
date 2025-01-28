7e6fa27ec8718437978b30ecaeda8d206778e269
TehilaShaulzon <t8472959@gmail.com>
2025-01-28 15:31:22 +0200

    add GET to meetings

--- server/controllers/meetingsController.ts
+++ server/controllers/meetingsController.ts
@@ -1,10 +1,22 @@
-import { addMeeting, updateMeeting } from "../services/meetingsService";
+import { addMeeting, getMeetings, updateMeeting } from "../services/meetingsService";
 import { Request, Response } from 'express';
 import {  CustomError } from "../errors/CustomError"
 
 const express = require('express');
 const router = express.Router();
 
+router.get('/getMeetings', async (req: Request, res: Response) => {
+    try {
+        const result = await getMeetings();
+        res.status(200).json(result);
+    } catch (err: any) {
+        if (err instanceof CustomError) {
+            res.status(err.statusCode).send(err.message);
+        } else {
+            res.status(500).send('Internal Server Error');
+        }
+    }
+});
 router.post('/addMeeting', async (req: Request, res: Response) => {
     try {
         const result = await addMeeting(req.body);
