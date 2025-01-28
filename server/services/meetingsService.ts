02c8f3ec24d6bc4c548d9d834748373ccdbff9f3
TehilaShaulzon <t8472959@gmail.com>
2025-01-28 13:35:19 +0200

    add update meeting to service

--- server/services/meetingsService.ts
+++ server/services/meetingsService.ts
@@ -7,43 +7,90 @@ import { User } from "../models/user";
 import { isValidMeetingDate } from "../validators/validators";
 
 export async function addMeeting(newMeeting: Meetings) {
-    const service = await Services.findByPk(newMeeting.serviceId);
-    if (!service) {
-      throw new CustomError('Service with this Id not found',404);
-    }
-    const user = await User.findByPk(newMeeting.userId);
-    if (!user) {
-      throw new CustomError('User with this Id not found',404);
-    }
-    if(!isValidMeetingDate(newMeeting.meetingDate))
-        throw new CustomError('Error in meeting date',400);
-    const endTime = new Date(newMeeting.meetingDate);
-    endTime.setMinutes(endTime.getMinutes() + service.serviceDuration);
-
-    const conflictingMeetings = await Meetings.findOne({
-      where: {
-        meetingDate: {
-          [Op.lt]: endTime,
-          [Op.gt]: new Date(newMeeting.meetingDate).setMinutes(newMeeting.meetingDate.getMinutes() - service.serviceDuration),
-        },
-      },
-    });
-
-    if (conflictingMeetings) {
-      throw new CustomError('The selected time slot is already booked',409);
-    }
-    await sequelize.authenticate();
-    await Meetings.sync();
-
-    const meeting = await Meetings.create({
-        meetingDate: newMeeting.meetingDate,
-        userId: newMeeting.userId,
-        serviceId:newMeeting.serviceId
-    });
-
-    const result = await Meetings.findByPk(meeting.id, {
-        attributes: { exclude: ['createdAt', 'updatedAt'] }
-    });
-
-    return result;
+  const service = await Services.findByPk(newMeeting.serviceId);
+  if (!service) {
+    throw new CustomError('Service with this Id not found', 404);
   }
+  const user = await User.findByPk(newMeeting.userId);
+  if (!user) {
+    throw new CustomError('User with this Id not found', 404);
+  }
+  if (!isValidMeetingDate(newMeeting.meetingDate))
+    throw new CustomError('Error in meeting date', 400);
+  const endTime = new Date(newMeeting.meetingDate);
+  endTime.setMinutes(endTime.getMinutes() + service.serviceDuration);
+
+  const conflictingMeetings = await Meetings.findOne({
+    where: {
+      meetingDate: {
+        [Op.lt]: endTime,
+        [Op.gt]: new Date(newMeeting.meetingDate).setMinutes(newMeeting.meetingDate.getMinutes() - service.serviceDuration),
+      },
+    },
+  });
+
+  if (conflictingMeetings) {
+    throw new CustomError('The selected time slot is already booked', 409);
+  }
+  await sequelize.authenticate();
+  await Meetings.sync();
+
+  const meeting = await Meetings.create({
+    meetingDate: newMeeting.meetingDate,
+    userId: newMeeting.userId,
+    serviceId: newMeeting.serviceId
+  });
+
+  const result = await Meetings.findByPk(meeting.id, {
+    attributes: { exclude: ['createdAt', 'updatedAt'] }
+  });
+
+  return result;
+}
+
+
+export async function updateMeeting(meetingId: number, updatedMeeting: Meetings) {
+
+  const meeting = await Meetings.findOne({
+    where: { id: meetingId }
+  });
+
+  if (!meeting) {
+    throw new CustomError('meeting not found', 404);
+  }
+
+  if (meeting.userId !== updatedMeeting.userId) {
+    throw new CustomError('Cannot update userId for an existing meeting', 400);
+  }
+  if (meeting.serviceId !== updatedMeeting.serviceId) {
+    throw new CustomError('Cannot update serviceId for an existing meeting', 400);
+  }
+  const service = await Services.findByPk(meeting.serviceId);
+  if (!service) {
+    throw new CustomError('Service with this Id not found', 404);
+  }
+  if (!isValidMeetingDate(updatedMeeting.meetingDate))
+    throw new CustomError('Error in meeting date', 400);
+  const endTime = new Date(updatedMeeting.meetingDate);
+  endTime.setMinutes(endTime.getMinutes() + service.serviceDuration);
+
+  const conflictingMeetings = await Meetings.findOne({
+    where: {
+      meetingDate: {
+        [Op.lt]: endTime,
+        [Op.gt]: new Date(updatedMeeting.meetingDate).setMinutes(updatedMeeting.meetingDate.getMinutes() - service.serviceDuration),
+      },
+    },
+  });
+
+  if (conflictingMeetings) {
+    throw new CustomError('The selected time slot is already booked', 409);
+  }
+  await meeting.update(updatedMeeting);
+
+  const { createdAt, updatedAt, ...result } = meeting.get();
+
+  return result;
+
+}
+
