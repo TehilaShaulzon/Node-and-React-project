1a232d46d27281683ba0d6dbfe3e53bfcb9392f6
TehilaShaulzon <t8472959@gmail.com>
2025-01-28 14:29:24 +0200

    add route to mmetings

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import setupSwagger from './swaggerConfig';
 import usersController from './controllers/usersController';
 import businessController from './controllers/businessController';
 import servicesController from './controllers/serviceController'
+import meetingsController from './controllers/meetingsController';
 dotenv.config()
 const port=process.env.PORT||8000
 const app= express();
@@ -14,6 +15,7 @@ setupSwagger(app);
 app.use('/users', usersController);
 app.use('/business', businessController);
 app.use('/services', servicesController);
+app.use('/meetings', meetingsController);
 
 app.listen(port, async () => {
     console.log(`Server is running at http://localhost:${port}/docs`);
