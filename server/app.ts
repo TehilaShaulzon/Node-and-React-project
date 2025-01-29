3676586537262c35407d4a9eb1815a510954795c
TehilaShaulzon <t8472959@gmail.com>
2025-01-29 21:37:31 +0200

    add global error handling middleware

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { User } from './models/user';
 import setupSwagger from './swaggerConfig';
 import usersController from './controllers/usersController';
 import businessController from './controllers/businessController';
 import servicesController from './controllers/servicesController';
 import meetingsController from './controllers/meetingsController';
 import { verifyToken } from './middlewares/verifyTokenMiddleware';
+import { Request, Response, NextFunction } from 'express';
+
 dotenv.config();
-const port=process.env.PORT||8000;
-const app= express();
+const port = process.env.PORT || 8000;
+const app = express();
 app.use(express.json());
 
 setupSwagger(app);
@@ -19,7 +20,20 @@ app.use('/business', businessController);
 app.use('/services', servicesController);
 app.use('/meetings', meetingsController);
 
-app.listen(port, async () => {
-    console.log(`Server is running at http://localhost:${port}/docs`);
+app.use("*", (req, res) => {
+    res.status(404).json({ error: "PAGE NOT FOUND" });
+});
 
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error("Error:", err);
+
+    if (err instanceof Error) {
+        res.status(500).json({ error: err.message });
+    } else {
+        res.status(500).json({ error: "Unknown error occurred" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/docs`);
 });
